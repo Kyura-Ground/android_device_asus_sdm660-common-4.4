7e02067e97f1c658927edb6f7895da457f494e42
ikwfahmi <mikhwanfahmi4@gmail.com>
2026-05-04 16:35:43 +0700

    sdm660-common: Inherit our signing key
    Signed-off-by: ikwfahmi <mikhwanfahmi4@gmail.com>
    
    # Conflicts:
    #	sdm660.mk

--- sdm660.mk
+++ sdm660.mk
@@ -7,6 +7,9 @@
 # Inherit the proprietary files
 $(call inherit-product, vendor/asus/sdm660-common/sdm660-common-vendor.mk)
 
+# Inherit our signing key
+$(call inherit-product-if-exists, vendor/lineage-priv/keys/keys.mk)
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_COMPRESSED_APEX := false
